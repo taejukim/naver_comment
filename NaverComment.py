e87c1aa6e920019069bd5180a897641a8ece391b
taeju.kim <iam@taeju.kim>
2021-06-16 23:44:46 +0900

    update naver comment

--- NaverComment.py
+++ NaverComment.py
@@ -28,22 +28,18 @@ class NaverComment:
         """
         self.cafe_id = 30020276
         self.comment_flag = False
-
-        if  getattr(sys, 'frozen', False): 
-            chromedriver_path = os.path.join(sys._MEIPASS, "chromedriver.exe")
-            self.driver = webdriver.Chrome(chromedriver_path)
-        else:
-            self.driver = webdriver.Chrome()
-
+        print('Loading chrome..')
+        self.driver = webdriver.Chrome(os.path.join(os.getcwd(), 'chromedriver.exe'))
+        print('Complete..')
         login_url = 'https://nid.naver.com/nidlogin.login?mode=form'
         self.driver.get(login_url)
-        input('로그인 후 엔터키를 입력해주세요....')
-        self.msg = input('댓글 내용을 입력해주세요 : ')
-        print('입력하하신 댓글의 내용은 "{}" 입니다.'.format(self.msg))
+        input('네이버 로그인 후 엔터키를 입력해주세요....')
+        self.msg = input('이름과 생일을 입력해주세요(홍길동0321) : ')
+        print('입력하신 이름과 생일은 "{}" 입니다.'.format(self.msg))
         self.cookies = self.get_cookies_for_requests(self.driver.get_cookies())
         self.url = 'https://cafe.naver.com/ArticleList.nhn?'
-        self.url +='search.clubid={}&search.menuid=2&search.boardtype=L'.format(self.cafe_id)
-        # https://cafe.naver.com/ArticleList.nhn?search.clubid=30020276&search.menuid=2&search.boardtype=L
+        self.url += 'search.clubid={}&search.menuid=2&search.boardtype=L'.format(self.cafe_id)
+        print('Move to Naver cafe..')
         self.driver.get(self.url)
 
     def get_cookies_for_requests(self, cookies):
@@ -67,28 +63,33 @@ class NaverComment:
         """
         if self.comment_flag:
             return
-        first_article = self.get_first_article()
-        print(datetime.now().strftime('[%y-%m-%d %H:%M:%S.%f]'),\
-             '게시글 ID {} 보다 최신 게시글 찾고 있습니다...'.format(target_id))
+        first_article, dongho = self.get_first_article()
+        print(datetime.now().strftime('[%y-%m-%d %H:%M:%S.%f]'), '게시글 ID {} 보다 최신 게시글 찾고 있습니다...'.format(target_id))
         if int(target_id) < int(first_article):
             if self.comment_flag:
                 return True
-            self.write_comment(first_article)
+            self.write_comment(first_article, dongho)
             print(datetime.now().strftime('[%y-%m-%d %H:%M:%S.%f]'), '완료', first_article)
             return True
     
     def get_first_article(self):
-        r = requests.get(
-            self.url, 
-            cookies = self.cookies,
-            )
+        r = requests.get((self.url),
+          cookies=(self.cookies))
         soup = bs(r.text, 'html.parser')
         tbodys = soup.find_all('tbody')
-        first_article = tbodys[1].find_all('div', {'class':'inner_number'})[0].text
-        time.sleep(0.01)
-        return first_article
+        first_article = tbodys[1].find_all('div', {'class': 'inner_number'})[0].text
+        title = tbodys[1].find_all('div', {'class': 'inner_list'})[0].text.strip()
+        try:
+            dong = re.search('\\d*?(?=동)', title).group().zfill(4)
+            ho = re.search('\\d*?(?=호)', title).group().zfill(4)
+        except:
+            return (
+             first_article, None)
+        else:
+            return (
+             first_article, dong + ho)
 
-    def write_comment(self, article_id):
+    def write_comment(self, article_id, dongho):
         """
         게시글에 댓글을 남기는 함수
         :param article_id: 댓글을 남길 게시글 ID
@@ -98,23 +99,19 @@ class NaverComment:
         if self.comment_flag:
             return True
         self.comment_flag = True
-        # idenifier = 'https://apis.naver.com/cafe-home-web/cafe-home/v1/member/identifier'
         comment_post = 'https://apis.naver.com/cafe-web/cafe-mobile/CommentPost.json'
-        # requests.get(idenifier, cookies=self.cookies)
-        comment_data = {
-             'content': self.msg,
-             'stickerId': '',
-             'cafeId': self.cafe_id,
-             'articleId': article_id,
-             'requestFrom': 'A'
-             }
-        comment = requests.post(
-                    comment_post,
-                    cookies=self.cookies,
-                    data=comment_data
-                    )
-        print(datetime.now().strftime('[%y-%m-%d %H:%M:%S.%f]'),\
-                     '댓글 작성 성공! ({})'.format(self.msg))
+        if dongho:
+            msg = dongho + self.msg
+        else:
+            msg = self.msg
+        comment_data = {'content':dongho + self.msg,  'stickerId':'', 
+         'cafeId':self.cafe_id, 
+         'articleId':article_id, 
+         'requestFrom':'A'}
+        comment = requests.post(comment_post,
+          cookies=(self.cookies),
+          data=comment_data)
+        print(datetime.now().strftime('[%y-%m-%d %H:%M:%S.%f]'), '댓글 작성 성공! ({})'.format(self.msg))
         self.enter_article(article_id)
         print('>>>>>>>>>>>>>>>>>>>>>>>>>>>> Ctrl + C 를 누르면 종료 됩니다.')
         return article_id
@@ -131,17 +128,13 @@ class NaverComment:
         self.driver.get(url)
         
 if __name__ == '__main__':
+    print('Start...')
     test = True
     comment = NaverComment()
     time.sleep(3)
-    # target_id = int(input('Target 게시글 ID를 입력해주세요(숫자) : '))
-    first_article = comment.get_first_article()
-    print('현재 게시판의 최신 글의 ID는 {} 입니다. \n 게시글 ID {} 보다 최신 게시글을 찾습니다.'\
-        .format(first_article, first_article))
+    first_article, _ = comment.get_first_article()
+    print('현재 게시판의 최신 글의 ID는 {} 입니다. \n 게시글 ID {} 보다 최신 게시글을 찾습니다.'.format(first_article, first_article))
     input('시작 하시려면 엔터키를 입력해주세요....')
-    
     while True:
-            t = threading.Thread(target=comment.search_target_article,
-                                args=(first_article,))
-            t.start()
+        comment.search_target_article(first_article)
     
